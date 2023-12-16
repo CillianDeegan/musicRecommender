c3500d08e811c2dffc1982af7aea98c0d362ef08
CillianDeegan <140992507+CillianDeegan@users.noreply.github.com>
2023-12-16 16:59:12 +0000

    server

---
+++ server.py
@@ -0,0 +1,57 @@
+import datetime
+import requests
+import urllib.parse
+
+from flask import Flask, redirect, request, jsonify, session
+
+app = Flask(__name__)
+
+CLIENT_ID = 'bdf6987c1b984d4c814218ccbf89bc0e'
+CLIENT_SECRET = '1feebe62668e46e49a1afab95d452511'
+REDIRECT_URI = 'http://localhost:5000/callback'
+
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+API_BASE_URL = 'https://api.spotify.com/v1/'
+
+@app.route('/')
+def index():
+    return "Welcome <a href='/login'>login</a>"
+
+@app.route('/login')
+def login():
+    scope = 'user-read-private user-read-email'
+
+    params = {
+        'client_id': CLIENT_ID,
+        'response_type': 'code',
+        'scope': scope,
+        'redirect_uri': REDIRECT_URI,
+        'show_dialog': True
+    }
+
+    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    if 'error' in request.args:
+        return jsonify({"error": request.args['error']})
+    
+    if 'code' in request.args:
+        req_body = {
+            'code': request.args['code'],
+            'grant_type': 'authorization_code',
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+
+        response = requests.post(TOKEN_URL, data=req_body)
+        token_info = response.json()
+
+        session['access_token'] = token_info['access_token']
+        session['refresh_token'] = token_info['refresh_token']
+        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in'] 
+
